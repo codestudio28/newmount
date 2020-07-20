69c4986c4f58c8de5e40e89b91f415b69ab5e831
cruz-jerwin15 <cruz.jerwin15@gmail.com>
2020-07-20 14:18:20 +0800

    072020207

--- app/Http/Controllers/PropertyCustomController.php
+++ app/Http/Controllers/PropertyCustomController.php
@@ -167,7 +167,7 @@ class PropertyCustomController extends Controller
           $miscs = Misc::where('client_id',$client_id)->where('property_id',$property_id)
           ->where('status','PAID')->orderBy('id','desc')->first();
 
-          if(count($equities)<=0){
+          if(count($miscs)<=0){
             $totalmisc =$to_misc;
           }else{
             $totalmisc =$mics[0]->balance;
