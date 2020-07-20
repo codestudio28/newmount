62ae6c30bd88a7bd2afb84cd18689cb2e9de8e75
cruz-jerwin15 <cruz.jerwin15@gmail.com>
2020-07-20 15:37:09 +0800

    0720202018

--- app/Http/Controllers/ReportClientController.php
+++ app/Http/Controllers/ReportClientController.php
@@ -72,13 +72,13 @@ class ReportClientController extends Controller
               array_push($equity_bal,"Php. ".number_format($buy->property->equity[$equity_count-1]->balance,2));
                array_push($misc_bal,"Php. ".number_format($buy->property->misc[$misc_count-1]->balance,2));
              if($misc_count<=1){
-                  array_push($misc_pen,"Php. ".number_format('0',2));
+                  array_push($misc_pen,"Php. ".number_format(0,2));
              }else{
                  array_push($misc_pen,"Php. ".number_format($buy->property->misc[$misc_count-2]->penalty,2));
              }
-               return $equity_count;
+              
              if($equity_count<=1){
-                 array_push($equity_pen,"Php. ".number_format('0',2));
+                 array_push($equity_pen,"Php. ".number_format(0,2));
              }else{
                 return $buy->property->equity[$equity_count-2]->penalty;
                  // array_push($equity_pen,"Php. ".number_format($buy->property->equity[$equity_count-2]->penalty,2));
